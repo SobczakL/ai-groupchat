ea04349930cde076aac87a5148e76300111efa7a
Lucas sobczak <lucassobczak3@gmail.com>
2025-02-25 20:40:54 -0500

    setup tailwind in client

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -5,24 +5,38 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    { ignores: ['dist'] },
+    {
+        extends: [
+            js.configs.recommended,
+            // If you add eslint-plugin-react later, you might extend its recommended config:
+            // "plugin:react/recommended",
+            ...tseslint.configs.recommended,
+        ],
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
+        settings: {
+            react: {
+                version: 'detect'
+            },
+        },
+        plugins: {
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+            // If you install eslint-plugin-react, add it here:
+            // react: require('eslint-plugin-react'),
+        },
+        rules: {
+            // Disable the rule requiring React in scope—this is needed with React 17+
+            'react/react-in-jsx-scope': 'off',
+            ...reactHooks.configs.recommended.rules,
+            'react-refresh/only-export-components': [
+                'warn',
+                { allowConstantExport: true },
+            ],
+        },
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
 )
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css'
+import { Button } from "@/components/ui/button"
 
 function App() {
 
     return (
-        <>
-        </>
+        <div className='h-[100vh]'>
+            <p className='mx-16 text-black'>hello!</p>
+            <Button>button</Button>
+        </div>
     )
 }
 
